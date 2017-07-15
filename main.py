787a92c44f01abc849c24edeee1bb6b6e5b363b9
TrisKast <tristan.kast@gmx.de>
2017-07-16 01:45:12 +0200

    Add files via upload
    This main.py is able to switch between alice and bob

---
+++ main.py
@@ -0,0 +1,41 @@
+#!flask/bin/python
+from flask import Flask, render_template, request, make_response, url_for, abort, session
+import os
+import sys
+import json
+import random
+import pandas as pd
+
+app = Flask(__name__)
+
+# read in vehicle data
+def makedataAlice00():
+    with open('example.json') as data_file:
+        #data = pd.read_csv(data_file, ';')
+        data = json.load(data_file)
+    return data
+
+def makedataBob00():
+    with open('Bob00.json') as data_file:
+        #data = pd.read_csv(data_file, ';')
+        data = json.load(data_file)
+    return data
+
+# default route
+@app.route('/')
+def main():
+    return render_template('index.html', data=json.dumps(makedataAlice00()))
+
+# route for bubbleChart
+@app.route('/zoomchartAlice')
+def zoomchartAlice():
+    return render_template('zoomchartAlice.html', data=json.dumps(makedataAlice00()))
+
+@app.route('/zoomchartBob')
+def zoomchartBob():
+    return render_template('zoomchartBob.html', data=json.dumps(makedataBob00()))
+
+
+if __name__ == '__main__':
+    # main()
+    app.run(debug=True)
